e0a42c6a372f08b47775b909b78fa53473661280
vitoroberto <vitor.roberto.dev@gmail.com>
2023-05-29 20:17:44 -0300

    perf: change image format

--- src/data/info.ts
+++ src/data/info.ts
@@ -3,7 +3,7 @@ export const info = {
     {
       id: 1,
       name: "Meu Portfólio",
-      thumbnail: "/assets/images/portfolio-image.png",
+      thumbnail: "/assets/images/portfolio-image.webp",
       liveUrl: "/",
       githubUrl: "https://github.com/vitoroberto/vitor-portfolio",
       alt: "Site do meu Portfólio",
@@ -11,7 +11,7 @@ export const info = {
     {
       id: 2,
       name: "got-quotes",
-      thumbnail: "/assets/images/got-quotes-image.png",
+      thumbnail: "/assets/images/got-quotes-image.webp",
       liveUrl: "https://quotes-got.vercel.app/",
       githubUrl: "https://github.com/vitoroberto/got-quotes",
       alt: "Um site que apresenta algumas citações da série Game of Thrones.",
