d27cc06c4e66076feb159d4243d439b4b174e51e
vitoroberto <73672691+vitoroberto@users.noreply.github.com>
2024-07-02 23:12:58 -0300

    feat: add email copy button

--- src/components/SocialItem.tsx
+++ src/components/SocialItem.tsx
@@ -8,18 +8,16 @@ type propTypes = {
 
 export function SocialItem({ ...props }: propTypes) {
   return (
-    <ul>
-      <li>
-        <a
-          className="flex items-center gap-2 rounded-md p-2 px-4 py-2 duration-200 ease-out hover:scale-105 hover:bg-gray-200 hover:text-blue-700 active:scale-95 dark:hover:bg-neutral-800 dark:hover:text-purple-600"
-          href={props.href}
-          target="_blank"
-          rel="noreferrer noopener"
-          aria-label={props.ariaLabel}
-        >
-          {props.children}
-        </a>
-      </li>
-    </ul>
+    <li>
+      <a
+        className="flex items-center gap-2 rounded-md p-2 px-4 py-2 duration-200 ease-out hover:scale-105 hover:bg-gray-200 hover:text-blue-700 active:scale-95 dark:hover:bg-neutral-800 dark:hover:text-purple-600"
+        href={props.href}
+        target="_blank"
+        rel="noreferrer noopener"
+        aria-label={props.ariaLabel}
+      >
+        {props.children}
+      </a>
+    </li>
   );
 }
